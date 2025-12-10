765633fb0a11760c095453481f455c2d3cced7be
rwy685 <rwy685@gmail.com>
2025-12-10 17:24:45 +0900

    update.주석추가

--- Assets/01_Scripts/02_UI/UIRefactor/EventBus.cs
+++ Assets/01_Scripts/02_UI/UIRefactor/EventBus.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 
 public static class EventBus
 {
+    //구독자를 타입형태로 리스트로 관리
     private static readonly Dictionary<Type, IList> _subscribers = new();
 
+    //구독자가 실행할 리스트 불러오기
     private static List<WeakReference<Action<T>>> GetList<T>()
     {
         var type = typeof(T);
 
         if (!_subscribers.TryGetValue(type, out var list))
         {
-            list = new List<WeakReference<Action<T>>>();
+            list = new List<WeakReference<Action<T>>>(); // 리스트가 없다면 새로 생성
             _subscribers[type] = list;
         }
 
-        return (List<WeakReference<Action<T>>>)list;
+        return (List<WeakReference<Action<T>>>)list; //리스트가 있다면 유지
     }
 
+    //구독
     public static void Subscribe<T>(Action<T> callback)
     {
         GetList<T>().Add(new WeakReference<Action<T>>(callback));
     }
 
+    //해지
     public static void Unsubscribe<T>(Action<T> callback)
     {
         var list = GetList<T>();
@@ -38,6 +42,7 @@ public static class EventBus
         });
     }
 
+    //발행
     public static void Publish<T>(T eventData)
     {
         var list = GetList<T>();
@@ -52,6 +57,7 @@ public static class EventBus
                 continue;
             }
 
+            // 예외처리
             try
             {
                 callback.Invoke(eventData);
